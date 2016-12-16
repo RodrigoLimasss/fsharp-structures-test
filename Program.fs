f185473ac2e58e6fffa0df8619d547b346ce84f3
Rodrigo Lima <rodrigolimasss@gmail.com>
2016-12-16 13:34:55 -0200

    Adding gitIgnore

--- Program.fs
+++ Program.fs
@@ -1,7 +1,12 @@
 ﻿// Learn more about F# at http://fsharp.org
 // See the 'F# Tutorial' project for more help.
+module Program
+
+open System
 
 [<EntryPoint>]
-let main argv = 
-    printfn "%A" argv
+let main argv =
+    //printfn "%A" argv
+    printfn "%A" Arrays.lineArray
+    let key = Console.ReadKey()
     0 // return an integer exit code
